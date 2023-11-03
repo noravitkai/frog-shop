0df202e76c12ef7f61aaa034a7dc1bf747c57df2
GOULASHSUP <simjob01@easv365.dk>
2023-11-03 10:47:20 +0100

    Create AboutHeader component
    Creation of a header, including a title and introduction text to set the tone for the AboutPage.

---
+++ src/components/AboutHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bg-ghostwhite">
+    <div class="mx-auto max-w-2xl px-4 py-4 sm:px-6 sm:py-8 lg:max-w-7xl lg:px-8 lg:py-12">
+      <h2
+        class="text-2xl lg:text-4xl text-center font-bold tracking-tight text-hovergreen pb-3 sm:pb-4 lg:pb-6 relative mb-6 sm:mb-8 lg:mb-12"
+      >
+        Rólunk
+        <!-- Line Separator -->
+        <span
+          class="block w-16 mx-auto h-1 bg-hovergreen absolute bottom-0 left-1/2 transform -translate-x-1/2 mb-1 sm:mb-2"
+        ></span>
+      </h2>
+      <!-- Introduction -->
+      <p class="text-left sm:text-center text-shadowgreen">
+        Egy kicsi, de elszánt csapat vagyunk, akiknek célja, hogy fenntartható és egyedi termékeket
+        áruljanak mindenkinek! A termékeinket 100%-ban kézzel készítjük, hogy a legjobb és
+        legmegbízhatóbb kulcstartó, minineszeszer vagy táska kerüljön az asztalodra.
+      </p>
+    </div>
+  </div>
+</template>
